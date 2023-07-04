41a9e597e1ba5b48f03057e193d7e30d2e1cd97e
Kenny Morales <kennymorales580@gmail.com>
2023-07-04 02:54:14 -0500

    setting up basic functions

---
+++ script.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title; 
+    this.author = author;
+    this.pages = parseInt(pages);
+
+}
+
+function addBookToLibrary() {
+
+}
+
+function libraryDisplay() {
+    
+}
