b160d1780506eb6c7dbe523a6f523663611bd0b0
Kenny Morales <kennymorales580@gmail.com>
2023-07-04 03:26:24 -0500

    created save book functionality

--- script.js
+++ script.js
@@ -7,16 +7,20 @@ function Book(title, author, pages, read) {
     this.read = Boolean(read);
 }
 
-const hobbit = new Book('Hobbit', 'Kenny Morales', '12', 'true');
-console.log(hobbit.title);
-console.log(hobbit.author);
-console.log(hobbit.pages);
-console.log(hobbit.read)
 
 function addBookToLibrary() {
+    titleInput = prompt("What's the title?");
+    authorInput = prompt("Who's the author?");
+    pagesInput = prompt("How many pages does it have?");
+    readInput = prompt("Have you read it?");
 
+    bookInput = new Book(titleInput, authorInput, pagesInput, readInput);
+    myLibrary.push(bookInput);
+    console.log(myLibrary);
 }
 
 function libraryDisplay() {
     
 }
+
+addBookToLibrary();
