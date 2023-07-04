177e97cccb6ac074bd08d3bc33c45312d3b19b86
Kenny Morales <kennymorales580@gmail.com>
2023-07-04 02:59:27 -0500

    made it so the book takes in values

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ function Book(title, author, pages, read) {
     this.title = title; 
     this.author = author;
     this.pages = parseInt(pages);
-
+    this.read = Boolean(read);
 }
+
+const hobbit = new Book('Hobbit', 'Kenny Morales', '12', 'true');
+console.log(hobbit.title);
+console.log(hobbit.author);
+console.log(hobbit.pages);
+console.log(hobbit.read)
 
 function addBookToLibrary() {
 
